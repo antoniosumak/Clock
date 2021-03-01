691feaab1aca6c1e1f3711ffad184c79dba42932
Antonio Sumak <sumakantonio@gmail.com>
2021-03-01 23:15:20 +0100

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+function startClock() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("vrijeme").innerHTML = h + ":" + m + ":" + s;
+  setTimeout(startClock, 1000);
+  const postavke = { weekday: "long", month: "long", day: "numeric" };
+  document.getElementById("datum").innerText = today.toLocaleDateString(
+    "hr-HR",
+    postavke
+  );
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
